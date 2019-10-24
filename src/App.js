1ab017ed7a34fdcdb4f5818a6d47b6ccfbae6679
Chad <trileyschwarz@gmail.com>
2019-10-23 17:32:16 -0700

    Added example http request

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ import UncleSam from './assets/unclesam.png'
 import './App.css';
 
 function App() {
+
+
+        // create a new XMLHttpRequest
+        let xhr = new XMLHttpRequest();
+
+        // get a callback when the server responds
+        xhr.addEventListener('load', () => {
+            // update the state of the component with the result here
+            console.log(xhr.responseText)
+        })
+        // open the request with the verb and the url
+        xhr.open('GET', 'https://dog.ceo/api/breeds/list/all');
+        // send the request
+        xhr.send();
+
+
   return (
     <div className="App">
       <header className="App-header">
